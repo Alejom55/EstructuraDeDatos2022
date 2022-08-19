fc7414117f4c81a4fb82c52ebc44719cbce4e9af
Alejandro <91298541+Alejom55@users.noreply.github.com>
2022-08-19 12:55:28 -0500

    Update Ejercios_recursividad.py

--- Ejercios_recursividad.py
+++ Ejercios_recursividad.py
@@ -1,4 +1,4 @@
-# 1) Defina una funcion recursiva que reciba una lista para determinar cuantos numeros hay en ella
+""" 1) Defina una funcion recursiva que reciba una lista para determinar cuantos numeros hay en ella """
 l = [1,69,37,45,52,7,8]
 
 def f(lista):
@@ -12,7 +12,7 @@ print("Cantidad de elementos:", f(l))
 
 
 
-# 2) Defina una funcion recursiva que reciba una lista para determinar cuantos numeros pares hay en ella
+""" 2) Defina una funcion recursiva que reciba una lista para determinar cuantos numeros pares hay en ella """
 l = [1,2,3,4,5,6,8,10,12,11]
 
 def f(lista):
@@ -30,11 +30,22 @@ print("Cantidad de pares:", f(l))
 
 
 """ 3) Defina una función recursiva que reciba un string de números separados por coma, por ejemplo "1,2,3, 4,5" 
-       y devuelva la productoria de estos números. Tenga en cuenta el caso en que hay y no hay espacios entre los números y las comas."""
+y devuelva la productoria de estos números. Tenga en cuenta el caso en que hay y no hay espacios entre los números y las comas."""
 
 numeros = "1,2,3, 4,5"
 
 def f(string):
-    if string == "":
+    if (len(string) == 1):
+        return int(string)
+    elif (len(string) == 0):
         return 0
-    else: 
+    else:
+        ult = string[-1]
+        if (ult != " " and ult != ","):
+            ult = int(ult)
+            return ult * f(string[:-1])
+        else:
+            return f(string[:-1])
+
+
+print(f(numeros))
