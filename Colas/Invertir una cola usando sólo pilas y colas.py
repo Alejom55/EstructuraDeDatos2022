c2e52c455037f07cfca34db879bdea467874aac3
Alejandro <91298541+Alejom55@users.noreply.github.com>
2022-09-28 01:23:44 -0500

    Update Invertir una cola usando sólo pilas y colas.py

--- Colas/Invertir una cola usando sólo pilas y colas.py
+++ Colas/Invertir una cola usando sólo pilas y colas.py
@@ -1,3 +1,4 @@
+#Listas raras
 def invertir(self):
         l = [None]*self.N
         n = len(l)
@@ -6,3 +7,14 @@ def invertir(self):
 
         for i in range(self.N-1,-1,-1):
             self.enqueue(l[i])
+        
+#listas parcha3
+def invertir(self):
+        l = []
+        for i in self.queue:
+            l.append(i)
+        n = len(l)
+        for i in range(n-1,-1,-1):
+            self.enqueue(l[i])
+        for i in range(n):
+            l[i] = self.dequeue()
