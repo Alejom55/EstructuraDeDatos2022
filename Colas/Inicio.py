75d46ab4bfde0c01dd0b53f0712d5f138f6426ec
Alejandro <91298541+Alejom55@users.noreply.github.com>
2022-09-28 01:35:42 -0500

    Update Inicio.py

--- Colas/Inicio.py
+++ Colas/Inicio.py
@@ -34,3 +34,28 @@ class Queue:
     def __len__(self): #dunder method
         #retorna tamaño
         return len(self.queue)
+
+#inicio chill
+class Queue:
+    def __init__(self):
+        self.queue = []   
+    def print_queue(self):
+        print(self.queue)
+    def enqueue(self, e):
+        #agregar un elemento e a la cola
+        self.queue.append(e)  
+    def dequeue(self):
+        #remueve y retorna el primer elemento
+        if(self.is_empty()):
+            raise Exception("Cola vacía...")
+        return self.queue.pop(0)  
+    def first(self):
+        #retornar el primer elemento de la cola
+        if(self.is_empty()):
+            raise Exception("Cola vacía...")
+        return self.queue[0]  
+    def is_empty(self):
+        return len(self.queue)==0 
+    def __len__(self): #dunder method
+        #retorna tamaño
+        return len(self.queue)
