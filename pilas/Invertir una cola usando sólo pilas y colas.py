f202050b49633221858801d1a66542249f3eac7b
Alejandro <91298541+Alejom55@users.noreply.github.com>
2022-09-27 20:46:27 -0500

    Delete Invertir una cola usando sólo pilas y colas.py

--- pilas/Invertir una cola usando sólo pilas y colas.py
+++
@@ -1,8 +0,0 @@
-def invertir(self):
-        l = [None]*self.N
-        n = len(l)
-        for i in range(self.N):
-            l[i] = self.dequeue()
-
-        for i in range(self.N-1,-1,-1):
-            self.enqueue(l[i])
